1b757c08851ec6284a193ac2ec177e09f407ab2f
nguyenhuong791123 <nguyenhuong791123@gmail.com>
2019-11-26 00:10:14 +0900

    sftp send file

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import os
 import datetime
 import shutil
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, make_response
 from flask_cors import CORS
 from utils.sftp import *
 
@@ -23,8 +23,8 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/send', methods=[ 'POST' ])
-def send():
+@app.route('/putsftp', methods=[ 'POST' ])
+def putsftp():
     authorization = request.authorization
     # print(authorization)
 
@@ -43,12 +43,48 @@ def send():
     auth = {}
     auth['host'] = 'sc-sftp-01'
     auth['port'] = 22
-    auth['username'] = 'huongnv'
-    auth['password'] = 'Nguyen080!'
+    auth['username'] = 'sftp01'
+    auth['password'] = 'sftp01'
     # auth['password'] = './keys/id_rsa_sftp'
 
-    result = transport(auth, files)
+    result = transport_sftp(auth, files)
     return jsonify(result), 200
+
+@app.route('/getsftp', methods=[ 'GET' ])
+def getsftp():
+    authorization = request.authorization
+    # print(authorization)
+
+    auth = {}
+    auth['host'] = 'sc-sftp-01'
+    auth['port'] = 22
+    auth['username'] = 'sftp01'
+    auth['password'] = 'sftp01'
+
+    file = {}
+    file['path'] = '/home/sftp01/20191125142219.644'
+    file['filename'] = 'img.png'
+    file['flag'] = 'json'
+
+    result = {}
+    obj = download_sftp(auth, file)
+    filename = obj['filename']
+    local = obj['path'] + '/' + filename
+    if file['flag'] == 'file':
+        response = make_response()
+        response.data = open(local, 'rb').read()
+        response.headers['Content-Disposition'] = 'attachment; filename=' + filename
+        response.mimetype = 'image/png'
+        return response
+    elif file['flag'] == 'json':
+        if os.path.isfile(local):
+            result['filename'] = filename
+            result['msg'] = obj['msg']
+            result['data'] = str(convert_file_to_b64_string(local))
+            return jsonify(result), 200
+        return jsonify(result), 200
+    else:
+        return jsonify(result), 200
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8083)
--- utils/sftp.py
+++ utils/sftp.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
+import shutil
 import datetime
+import base64
 import paramiko
 # paramiko.util.log_to_file('/tmp/paramiko.log')
 
-def transport(auth, files):
-    print('Send File Start !!!')
+def transport_sftp(auth, files):
+    print('Transport File Start !!!' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     transport = paramiko.Transport((auth['host'], int(auth['port'])))
     transport.connect(username = auth['username'], password = auth['password'])
     sftp = paramiko.SFTPClient.from_transport(transport)
@@ -28,15 +30,13 @@ def transport(auth, files):
         remote = '/home/' + auth['username'] + '/' + dir
 
         obj = {}
-        obj['name'] = local
+        obj['local'] = local
         if mkdir_remote(sftp, remote):
             try:
+                # sftp.chmod(remote, mode=777)
                 if os.path.isfile(local):
-                    print(os.getcwd())
-                    print(file['local'])
-                    print(file['remove'])
                     sftp.put(local, filename)
-                    obj['msg'] =  local + 'へ' + remote + '/' + filename + 'の送信完了。'
+                    obj['remote'] = remote + '/' + filename
                 else:
                     raise IOError('Could not find localFile %s !!' % local)
             except IOError as err:
@@ -46,52 +46,70 @@ def transport(auth, files):
                     sftp.close()
                 if transport is not None:
                     transport.close()
+                    obj['msg'] =  '「' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '」転送完了。'
+                    print('Transport File End !!!' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                if outpath is not None and os.path.isdir(outpath):
+                    shutil.rmtree(outpath)
         else:
             obj['msg'] = 'Can not create dir to remote !!!'
         
         result.append(obj)
-
-    # client = None
-    # sftp_connection = None
-    # try:
-    #     client = paramiko.SSHClient()
-    #     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #     client.connect(file['host'], int(file['port']), file['username'], file['password'])
-    #     sftp_connection = client.open_sftp()
-    #     # ホームディレクトリのファイル一覧をprint
-    #     files = sftp_connection.listdir()
-    #     for remote_file in files:
-    #         print(remote_file)
-    #     # ファイルを取得
-    #     # sftp_connection.get('/path/to/remotefile', '/path/to/local')
-    #     # ファイルを転送
-    #     sftp.put('img.png', '/home/sftp01/' + file['filename'])
-    #     # sftp_connection.put(file['local'], file['remove'])
-    # except IOError as err:
-    #     obj['msg'] = str(err)
-    # finally:
-    #     if client:
-    #         client.close()
-    #     if sftp_connection:
-    #         sftp_connection.close()
-
     return result
 
+def download_sftp(auth, file):
+    print('Download File Start !!!' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    transport = paramiko.Transport((auth['host'], int(auth['port'])))
+    transport.connect(username = auth['username'], password = auth['password'])
+    sftp = paramiko.SFTPClient.from_transport(transport)
+
+    dt = datetime.datetime.now()
+    dir = dt.strftime('%Y%m%d%H%M%S.%f')[:-3]
+    downdir = 'download/'
+    outpath = downdir + dir
+    if os.path.isdir(outpath) == False:
+        os.mkdir(outpath)
+
+    filename = file['filename']
+    local = outpath + '/' + filename
+    remote = file['path'] + '/' + filename
+    obj = {}
+    obj['remote'] = remote
+    try:
+        sftp.get(remote, local)
+        obj['path'] = outpath
+        obj['filename'] = filename
+        obj['msg'] =  '「' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '」ダウンロード完了。'
+    except IOError as err:
+        obj['msg'] = str(err)
+    finally:
+        if sftp is not None:
+            sftp.close()
+        if transport is not None:
+            transport.close()
+            print('Download File End !!!' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    return obj
+
 def mkdir_remote(sftp, remote_directory):
-    """Change to this directory, recursively making new folders if needed.
-    Returns True if any folders were created."""
     if remote_directory == '/':
-        # absolute path so change directory to root
         sftp.chdir('/')
         return
     if remote_directory == '':
-        # top-level relative directory must exist
         return
     try:
-        sftp.chdir(remote_directory) # sub-directory exists
+        sftp.chdir(remote_directory)
     except IOError:
         dirname, basename = os.path.split(remote_directory.rstrip('/'))
-        mkdir_remote(sftp, dirname) # make parent directories
-        sftp.mkdir(basename) # sub-directory missing, so created it
+        mkdir_remote(sftp, dirname)
+        sftp.mkdir(basename)
         sftp.chdir(basename)
         return True
+
+def convert_file_to_b64_string(file_path):
+    with open(file_path, "rb") as f:
+        return base64.b64encode(f.read())
+
+
+def convert_b64_string_to_file(s, outfile_path):
+    with open(outfile_path, "wb") as f:
+        f.write(base64.b64decode(s))
